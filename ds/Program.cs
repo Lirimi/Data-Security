0fbabd23cb27bebbea78abad3390118ebbf6feed
Gentrit Ibishi <gentrit.ibishi@studentet.uni-pr.edu>
2020-03-12 23:07:36 +0100

    Commit All

---
+++ ds/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plaintekst = "gi";
+            string teksti = "Gentrit Ibishi";
+            string[] TekstiArray = teksti.Split(" ");
+            char[] ch = plaintekst.ToCharArray();
+            for (int i = 0; i <= TekstiArray.Length; i++)
+            {
+
+                //...
+
+            }
+
+
+        }
+    }
+}
