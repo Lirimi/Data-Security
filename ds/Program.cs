2edaf13b1f4a22e67afcea8d4a21bc669277de3b
Gentrit Ibishi <gentrit.ibishi@studentet.uni-pr.edu>
2020-03-12 23:39:37 +0100

    Commit All

--- ds/Program.cs
+++ ds/Program.cs
@@ -7,14 +7,16 @@ namespace ds
         static void Main(string[] args)
         {
             string plaintekst = "gi";
-            string teksti = "Gentrit Ibishi";
+            string teksti = "gentrit ibishi";
             string[] TekstiArray = teksti.Split(" ");
             char[] ch = plaintekst.ToCharArray();
-            for (int i = 0; i <= TekstiArray.Length; i++)
+            if (!(TekstiArray[0].StartsWith(ch[0])))
             {
-
-               //
-
+                Console.WriteLine("Nuk Starton");
+            }
+            else
+            {
+                Console.WriteLine(ch[0]);
             }
 
 
