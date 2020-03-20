2d06d9b0e951f78c7c9c907ce3b3fee6b3668595
LirimBeka <beka.lirim@gmail.com>
2020-03-20 20:24:06 +0100

    Implementimi i Numerikal ne Program.cs

--- ds/Program.cs
+++ ds/Program.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ds
 {
@@ -7,6 +11,20 @@ namespace ds
         static void Main(string[] args)
         {
             BealeEncrypt();
+            Console.Write("\nShenoni tekstin qe doni ta enkodoni: ");
+            string teksti =Console.ReadLine();
+            string enk = "";
+            string n = EncodeNumerical(teksti, enk);
+            Console.Write("Teksti i enkoduar eshte: " + n + "\n");
+         
+            Console.Write("Shkruaj kodin qe doni ta dekodoni: ");
+            string dek = Console.ReadLine();
+            int gjat = dek.Length;
+            string z = DecodeNumerical(dek, gjat);
+            //Decode(dek, gjat);
+            Console.Write("Kodi i dekoduar eshte: " + z + "\n");
+            Console.Write("\n");
+            
         }
 
         public static void BealeEncrypt()
@@ -20,7 +38,7 @@ namespace ds
             Console.Write("Shenoni plaintextin: ");
             string plaintekst = Console.ReadLine();
             char[] ch = plaintekst.ToCharArray();
-
+            Console.Write("Teksti i enkriptuar eshte: ");
             //for loop 
             for (int i = 0; i < plaintekst.Length; i++)
             {
@@ -39,7 +57,53 @@ namespace ds
             }
         }
 
+        static string EncodeNumerical(string teksti, string enk)
+        {
+            for (int i = 0; i < teksti.Length; ++i)
+            {
+                if (teksti[i]>=(int)'a' && teksti[i]<=(int)'z')
+            {
+                char ch = teksti[i];
+                if (!string.IsNullOrEmpty(enk))
+                    {
+                    enk += " ";
+                    }
+                int n = (int)ch- (int)'a' + 1;
 
 
+                enk += Convert.ToInt32(n);
+            }
+            else
+            {
+                continue;
+            }
+            }
+
+            return new string(enk);
+        }
+
+        static string DecodeNumerical(String dek, int gjat)
+        {
+            StringBuilder Decode = new StringBuilder(dek);
+            //Console.Write("Kodi i dekoduar eshte: ");
+            int num = 0;
+            for (int i = 0; i < gjat; i++)
+            {
+                num = (dek[i] - '1') + (int)'a';
+                if (num >= (int)'a' && num <= (int)'z')
+                {
+                    char c = (char)num;
+                    Decode[i] = c;
+                    //Console.Write(c);
+                    //num = 0;
+                }
+            }
+            //Console.Write("\n");
+            return Decode.ToString();
+        }
     }
 }
+
+
+
+
