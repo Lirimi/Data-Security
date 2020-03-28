eb55d8d76fcc1d687d2a7f2cc155cf4606bf0907
GentritIbishi <44057937+GentritIbishi@users.noreply.github.com>
2020-03-28 17:59:49 +0100

    Permisim te udhezimet!
    Permisim te udhezimet!

--- ds/Program.cs
+++ ds/Program.cs
@@ -15,9 +15,9 @@ namespace ds
             if (args.Length == 0)
             {
                 Console.WriteLine("\n@Argumentet mungojne!");
-                Console.WriteLine("\n@Per ekzekutimin e kodit Beale shtyp | Beale Encrypt <text> | ose | Beale Decrypt <text> ku text te decrypt duhet te jete me thonjeza like: 0 1 2 |");
-                Console.WriteLine("\n@Per ekzekutimin e kodit Permutation shtyp | Permutation Encrypt <key><text> | ose | Permutation Decrypt <key><text> |");
-                Console.WriteLine("\n@Per ekzekutimin e kodit Numerical shtyp | Numerical Encode <text> | ose | Numerical Decode <text> |");
+                Console.WriteLine("\n@Per ekzekutimin e kodit Beale shtyp | ds.exe Beale Encrypt <text> | ose | ds.exe Beale Decrypt <text> ku text te decrypt duhet te jete me thonjeza like: 0 1 2 |");
+                Console.WriteLine("\n@Per ekzekutimin e kodit Permutation shtyp | ds.exe Permutation Encrypt <key><text> | ose | ds.exe Permutation Decrypt <key><text> |");
+                Console.WriteLine("\n@Per ekzekutimin e kodit Numerical shtyp | ds.exe Numerical Encode <text> | ose | ds.exe Numerical Decode <text> |");
                 Environment.Exit(1);
 
             }
