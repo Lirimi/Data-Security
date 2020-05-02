44fa346fb440a77d86ae57812af559fb06fdbcdb
JonHajdini <jon_hajdini@hotmail.com>
2020-05-02 21:26:29 +0200

    Dekodimi i UserName dhe Qelsit me RSA

--- ds/DecryptMessage.cs
+++ ds/DecryptMessage.cs
@@ -18,15 +18,40 @@ namespace ds
             string key = ciphertextSplit[2];
             string encryptedMessage = ciphertextSplit[3];
         }
-        
-         
-        private string DecryptCiphertext(string ciphertext)
+        private string DecodeUserName(string username)
         {
-            byte[] byteCiphertext = Convert.FromBase64String(ciphertext);
-            byte[] byteDecryptedtext = objRSA.Decrypt(byteCiphertext, true);
+            /*-----Gets Bytes from String------*/
+            byte[] GetUserNameBytes = Convert.FromBase64String(username);
 
-            return Encoding.UTF8.GetString(byteDecryptedtext);
-            
+            /*-----Encode String from byte array----*/
+            string GetUserName = Encoding.UTF8.GetString(GetUserNameBytes);
+            return GetUserName;
         }
+
+        private byte[] DecryptKeywithRSA(string username, string key)
+        {
+            /*-----------Krijojme nje RSA instance--------*/
+            RSACryptoServiceProvider objRSA = new RSACryptoServiceProvider();
+
+            /*------Gets Bytes from key--------*/
+            byte[] GetKeyBytes = Convert.FromBase64String(key);
+
+            /*------Therrasim funksionin qe mundeson dekodimin e user-----*/
+            string user = DecodeUserName(username);
+
+            /*------Lexojme XML dokumentin ne strin------*/
+            string strXmlParameters = "";
+            StreamReader sr = new StreamReader("keys//" + user + ".xml");
+            strXmlParameters = sr.ReadToEnd();
+            sr.Close();
+            /*------E lexojme stringun ne instance-----*/
+            objRSA.FromXmlString(strXmlParameters);
+
+            /*------Dekriptojme qelsin-------*/
+            byte[] Decryptedkey = objRSA.Decrypt(GetKeyBytes, true);
+
+            return Decryptedkey;
+        }
+
     }
 }
